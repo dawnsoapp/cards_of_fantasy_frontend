7c11c677b4c141e0c6ec8662611a32698c3b916c
Madison Jackson <madi.jackson29@gmail.com>
2023-07-28 15:28:54 -0700

    working on submit name form. Was able to filter through texts via state

--- cards-of-fantasy/src/pages/dawn.js
+++ cards-of-fantasy/src/pages/dawn.js
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import '../components/mininav.css';
 import Mininav from '../components/mininav'
 
-const Dawn = () => {
+
+function Dawn() {
+
+    const [username, setUsername] = useState("");
+    const [currentPrompt, setCurrentPrompt] = useState(0);
+
+    const dawn = "Dawn";
+    const user = username;
+
+    let story = [
+        {   
+            storyPrompt: 0,
+            speaker: dawn,
+            user: username,
+            text: "Hello there! My name is Dawn! Dawn Soap. Not sponsored by the dishsoap by the way! I'm a certified master of the Astrologian class."
+        },
+        {
+            storyPrompt: 0,
+            speaker: dawn,
+            user: username,
+            text: "What's your name?"
+        },
+        {
+            storyPrompt: 0,
+            speaker: dawn,
+            user: username,
+            text: "Hello" + {user} + " !"
+        }
+    ]
+    const [newText, setNewText] = useState(0)
+// const [optionsCurrentPrompt, setOptionsCurrentPrompt] = useState(0);
+
+    const clickText = () => {
+        if ((story[newText].storyPrompt) === currentPrompt) {
+            setNewText(newText + 1);
+            return story[newText].text;
+        };
+    }
+
+    const submitName = () => {
+        if (user) {
+            setUsername(user);
+        }
+        
+        return console.log(username);
+    }
+
+
     return (
         <div className="App">
         <header className="Page-header">
         <Mininav />
         <p className="game-title">Chat with Dawn!</p>
         </header>
+        <p onClick={() => clickText()}>{story[newText].text}</p>
+        <label>
+            Name: <input name={user} type="string"/>
+        </label>
+        <button onClick={submitName}>Submit</button>
+        <p>Your Name: {username}</p>
         </div>
         
     )
