39f9f80ea2653a7b3bf9934d3c35eb202d9defa5
Madison Jackson <madi.jackson29@gmail.com>
2023-08-03 14:39:45 -0700

    implemented images, got image slider package to work from alice, clickable images to links for ffxiv

--- cards-of-fantasy/src/pages/ffxiv.js
+++ cards-of-fantasy/src/pages/ffxiv.js
@@ -2,8 +2,19 @@ import React from "react";
 import './ffxiv.css';
 import '../components/mininav.css';
 import Mininav from '../components/mininav'
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import dawntrail from '../imgs/dawntrail.jpeg';
+import fanfest from '../imgs/fanfest.jpeg';
+import community1 from '../imgs/community1.jpeg';
+import community2 from '../imgs/community2.png';
 
-const FFXIV = () => {
+function FFXIV() {
+
+    const handleLinkClick = (event, message) => {
+        console.log('Link element clicked');
+        console.log(message);
+    }
     return (
         <div className="App">
         <header className="Page-header">
@@ -11,7 +22,20 @@ const FFXIV = () => {
         <p className="game-title">Have you heard of the critically acclamined MMORPG...?</p>
         </header>
         <div className="slideshow-box">
-
+            <AliceCarousel autoplay autoPlayInterval="5" style="width: 500px">
+            <p>
+                <a 
+                href="https://na.finalfantasyxiv.com/dawntrail"
+                target="_blank"
+                rel="noreferrer">
+                <img src={dawntrail} className="sliderimg" alt=""/>
+                </a>
+            <footer>New expansion coming Summer 2024!</footer>
+            </p>
+            <p><img src={fanfest} className="sliderimg" alt=""/></p>
+            <p><img src={community1} className="sliderimg" alt=""/></p>
+            <p><img src={community2} className="sliderimg"alt=""/></p>
+            </AliceCarousel>
         </div>
         </div>
         
